bff3912c80232b29704b17c3345ff1a52795e1b1
Huy <danggiahuy147@gmail.com>
2023-05-15 19:49:59 +0700

    make handler error api response

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -2,6 +2,8 @@
 
 const AccessService = require("../services/access.service")
 
+const { OK, CREATED } = require('../core/success.response')
+
 class AccessController {
 
     signUp = async( req, res, next ) => {
@@ -10,7 +12,14 @@ class AccessController {
             200 OK
             201 CREATED
         */
-        return res.status(201).json(await AccessService.signUp(req.body))
+        new CREATED({
+            message: 'Registered OK!',
+            metadata: await AccessService.signUp(req.body),
+            options: {
+                limit: 10
+            }
+        }).send(res)
+        // return res.status(201).json(await AccessService.signUp(req.body))
     }
 }
 
